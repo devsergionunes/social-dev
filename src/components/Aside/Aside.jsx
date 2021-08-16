249f7fdf488476b1a57594f08079134d76b17fed
Paulo Sergio Nunes <paulo.sergio@lunes.io>
2021-08-16 09:25:24 -0300

    commit aside

--- src/components/Aside/Aside.jsx
+++ src/components/Aside/Aside.jsx
@@ -1,26 +1,26 @@
 import { AsideContent } from "./AsideStyle";
 
-import birthdayImg from '../../assests/imagens/bolo.png'
-import HourglassImg from '../../assests/imagens/Hourglass.png'
+import birthdayImg from "../../assests/imagens/bolo.png";
+import HourglassImg from "../../assests/imagens/Hourglass.png";
 
 export function Aside() {
   return (
     <AsideContent>
-      <section className='birthday'>
+      <section className="birthday">
         <div>
-          <img src={ birthdayImg } alt="Bolo de aniversario" />
+          <img src={birthdayImg} alt="Bolo de aniversario" />
           <h3>Aniversariantes</h3>
         </div>
         <p>Nenhum dos seus amigos comemora aniversario hoje</p>
       </section>
-      <section className='pendency'>
+      <section className="pendency">
         <div>
-          <img src={ HourglassImg } alt="Ampulheta do tempo" />
+          <img src={HourglassImg} alt="Ampulheta do tempo" />
           <h3>Pendências</h3>
         </div>
         <span>Seg, 21 de jun</span>
         <p>Voce nao possui nenhuma pendencia para hoje</p>
       </section>
     </AsideContent>
-  )
+  );
 }
